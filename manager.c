07b733cb5d65634d7d7d42a2c0875ff59b2fd2c8
HGU-jeremy <21900262@handong.ac.kr>
2022-04-18 20:52:37 +0900

    #3 saveData

--- manager.c
+++ manager.c
@@ -1,3 +1,12 @@
 #include <string.h>
 #include "product.h"
 
+void saveData(Product *p[], int count){
+    FILE *fp;
+    fp = fopen("product.txt", "wt");
+
+    for(int i = 0; i < count; i++){
+        if(p[i] == NULL) continue;
+        fprintf(fp, "%s\t %s\t %s\t %d %d\n", p[i]->name, p[i]->desc, p[i]->weight, p[i]->price, p[i]->check);
+    }
+
