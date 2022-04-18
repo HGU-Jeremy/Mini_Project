fcfb7ee9acd61c51d91f9da010d399857b141ce7
HGU-jeremy <21900262@handong.ac.kr>
2022-04-18 21:21:03 +0900

    #12 add more search functions in manager.h

--- manager.h
+++ manager.h
@@ -3,4 +3,5 @@
 void saveData(Product *p[], int count);
 int loadData(Product *p[]);
 void searchProduct(Product *p[], int count);
-
+void searchPrice(Product *p[], int count);
+void searchCheck(Product *p[], int count);
